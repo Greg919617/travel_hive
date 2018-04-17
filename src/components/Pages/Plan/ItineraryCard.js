e6a3fd593c77881ffbcf5ecdc5b5a4746cee0f4c
James Vaughn <jvaughn575@gmail.com>
2018-04-16 22:31:06 -0500

    Added initial Itinerary UI

---
+++ src/components/Pages/Plan/ItineraryCard.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import Itinerary from "./Itinerary"
+import Plan from "./index"
+
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Card, Col, Button } from 'antd';
+
+const { Meta } = Card;
+
+class ItineraryCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visible: true
+    };
+    this.delete = this.delete.bind(this);
+  }
+
+  delete = () => {
+    this.setState({
+      visible: false
+    });
+    this.props.remove(this.props.data);
+  }
+
+  render() {
+    return (
+      <div>
+        <Col span={8}>
+          <Card
+            hoverable
+            style = {{ margin: '5px' }}
+            title = {this.props.title}
+            visible = {this.state.visible}
+            cover = {<img src= {this.props.picture} height = '200px' />} >
+          <Meta
+            description = {this.props.description} />
+            <Button>
+              <Link to = {`/plan/${this.props.number}`}> Edit Itinerary </Link>
+            </Button>
+            <br/>
+            <Button
+              onClick = {this.delete}
+              type="danger"> Delete Itinerary
+            </Button>
+          </Card>
+        </Col>
+      </div>
+    )
+  }
+}
+
+export default ItineraryCard;
